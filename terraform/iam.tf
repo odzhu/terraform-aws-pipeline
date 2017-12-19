7019e617c3bd6d788a25873ee61f31360f56614e
Oleksii Dzhulai <Oleksii_Dzhulai@epam.com>
2017-12-19 16:35:15 +0200

    IAM for tf pipeline added

---
+++ terraform/iam.tf
@@ -0,0 +1,33 @@
+resource "aws_iam_role" "tf_pipeline" {
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "codepipeline.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy" "tf_pipeline" {
+  role = "${aws_iam_role.tf_pipeline.id}"
+
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": "*",
+            "Resource": "*"
+        }
+    ]
+}
+EOF
+}
